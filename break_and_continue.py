2d3764aba24921a27ef37d3542266e26328056ee
Juan Montoya <jcmontoya@intracitygeeks.org>
2018-05-04 21:46:12 +0000

    Module 3 done

---
+++ break_and_continue.py
@@ -0,0 +1,13 @@
+student_names = ["James", "Katarina", "Jessica", "Mark", "Bort", "Frank Grimes", "Max Power"]
+
+"""for name in student_names:
+    if name == "Mark":
+        print("Found him! " + name)
+        break
+    print("Currently testing " + name)"""
+    
+for name in student_names:
+    if name == "Bort":
+        continue
+        print("Found him! " + name)
+    print("Currently testing " + name)
