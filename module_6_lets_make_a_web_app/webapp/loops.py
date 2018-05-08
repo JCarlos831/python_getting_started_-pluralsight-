5059a1019c46eb8174fc86989fab7cc0c4caffd4
Juan Montoya <jcmontoya@intracitygeeks.org>
2018-05-08 19:17:05 +0000

    Finished Python: Getting Started Pluralsight Course

---
+++ module_6_lets_make_a_web_app/webapp/loops.py
@@ -0,0 +1,17 @@
+student_names = ["Mark", "Katarina", "Jessica", "Homer"]
+
+for name in student_names:
+    print("Student name is {0}".format(name))
+
+x = 0
+for index in range(10):
+    x += 10
+    print("The value of x is {0}".format(x))
+y = 0
+for index in range(5, 10):
+    y += 10
+    print("The value of y is {0}".format(y))
+z = 0    
+for index in range(5, 10, 2):
+    z += 10
+    print("The value of z is {0}".format(z))
